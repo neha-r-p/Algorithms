128300a1f563334ca63773b6135dbbe03c870945
Neha Patel <neha.r.p@gmail.com>
2019-12-12 17:01:20 -0800

    MVP eating cookies complete. followed TK and understood

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,9 +5,23 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
-cache = {0:1, 1:1, 2:2}
+
 def eating_cookies(n, cache=None):
-  pass
+    if n <= 1: 
+        return 1
+    elif n == 2: 
+        return 2
+    elif cache and cache[n] > 0: #if there is a cache and cache[n] is more than 0, return/stop the recursion
+        return cache[n]
+    else: #n > 2 and cache[n] == 0
+        if not cache:
+            cache = {i:0 for i in range(n+1)} #create cache of 0 for n numbers
+        cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+        return cache[n]
+
+print(eating_cookies(10))
+
+
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
